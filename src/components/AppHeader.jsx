75b3f48d8e73d06465d544476307bd5a44688b4f
Dmitriy <dyuzef@mail.ru>
2022-06-01 19:32:35 +0300

    end modal

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,19 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button, { SelectButton } from './Button';
-import styles from '../styles/modules/app.module.scss'
+import styles from '../styles/modules/app.module.scss';
 import TodoModel from './TodoModel';
 
-
 function AppHeader(props) {
+  const [modalOpen, setModalOpen] = useState(false);
+
   return (
     <div className={styles.appHeader}>
-      <Button variant='primary'>Add Task</Button>
+      <Button variant='primary' onClick={() => setModalOpen(true)}>
+        Add Task
+      </Button>
       <SelectButton id='status'>
         <option value='all'>All</option>
         <option value='incomplite'>Incomplite</option>
         <option value='complite'>Complite</option>
       </SelectButton>
-      <TodoModel/>
+      <TodoModel modalOpen={modalOpen} setModalOpen={setModalOpen} />
     </div>
   );
 }
