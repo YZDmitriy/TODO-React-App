a006489bc097eba91bdccedac04a6d5fc23e3802
Dmitriy <dyuzef@mail.ru>
2022-06-02 18:59:04 +0300

    add button delete for todo

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,8 +1,22 @@
+import { format } from 'date-fns/esm';
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { MdDelete, MdEdit } from 'react-icons/md';
 import styles from '../styles/modules/todoItem.module.scss';
 import { getClasses } from '../utils/getClasses';
+import { deleteTodo } from '../slices/todoSlices';
+import toast from 'react-hot-toast';
 
 function TodoItem({ todo }) {
+  const dispatch = useDispatch();
+
+  const handleDelete = () => {
+    dispatch(deleteTodo(todo.id));
+    toast.success('Todo Deleted Seccessfully')
+  };
+
+  const handleUpdate = () => {};
+
   return (
     <div className={styles.item}>
       <div className={styles.todoDetails}>
@@ -16,6 +30,29 @@ function TodoItem({ todo }) {
           >
             {todo.title}
           </p>
+          <p className={styles.time}>
+            {format(new Date(todo.time), 'p, MM/dd/yyyy')}
+          </p>
+        </div>
+      </div>
+      <div className={styles.todoActions}>
+        <div
+          className={styles.icon}
+          onClick={handleDelete}
+          onKeyDown={handleDelete}
+          tabIndex={0}
+          role='button'
+        >
+          <MdDelete />
+        </div>
+        <div
+          className={styles.icon}
+          onClick={handleUpdate}
+          onKeyDown={handleUpdate}
+          tabIndex={0}
+          role='button'
+        >
+          <MdEdit />
         </div>
       </div>
     </div>
