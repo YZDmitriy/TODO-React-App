06b345a665d37df6d98d28d06146ce99f317e4f9
Dmitriy <dyuzef@mail.ru>
2022-06-06 19:49:35 +0300

    done filter tasks

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -5,28 +5,32 @@ import TodoModel from './TodoModel';
 import { useDispatch, useSelector } from 'react-redux';
 import { updateFilterStatus } from '../slices/todoSlices';
 
-function AppHeader(props) {
+function AppHeader() {
   const [modalOpen, setModalOpen] = useState(false);
   const initialFilterStatus = useSelector((state) => state.todo.filterStatus);
   const [filterStatus, setFilterStatus] = useState(initialFilterStatus);
   const dispatch = useDispatch();
 
-
   const updateFilter = (e) => {
-    dispatch(updateFilterStatus(e.target.value))
+    setFilterStatus(e.target.value);
+    dispatch(updateFilterStatus(e.target.value));
   };
 
   return (
     <div className={styles.appHeader}>
-      <Button variant='primary' onClick={() => setModalOpen(true)}>
+      <Button variant="primary" onClick={() => setModalOpen(true)}>
         Add Task
       </Button>
-      <SelectButton id='status' value={filterStatus} onChange={updateFilter}>
-        <option value='all'>All</option>
-        <option value='incomplete'>Incomplete</option>
-        <option value='complete'>complete</option>
+      <SelectButton
+        id="status"
+        onChange={(e) => updateFilter(e)}
+        value={filterStatus}
+      >
+        <option value="all">All</option>
+        <option value="incomplete">Incomplete</option>
+        <option value="complete">Completed</option>
       </SelectButton>
-      <TodoModel type='add' modalOpen={modalOpen} setModalOpen={setModalOpen} />
+      <TodoModel type="add" modalOpen={modalOpen} setModalOpen={setModalOpen} />
     </div>
   );
 }
