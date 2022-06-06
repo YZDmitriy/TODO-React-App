04efbc0854238033a8857f899bcd89dc974c65f7
Dmitriy <dyuzef@mail.ru>
2022-06-06 19:30:54 +0300

    add filter todo tasks

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -2,16 +2,26 @@ import React, { useState } from 'react';
 import Button, { SelectButton } from './Button';
 import styles from '../styles/modules/app.module.scss';
 import TodoModel from './TodoModel';
+import { useDispatch, useSelector } from 'react-redux';
+import { updateFilterStatus } from '../slices/todoSlices';
 
 function AppHeader(props) {
   const [modalOpen, setModalOpen] = useState(false);
+  const initialFilterStatus = useSelector((state) => state.todo.filterStatus);
+  const [filterStatus, setFilterStatus] = useState(initialFilterStatus);
+  const dispatch = useDispatch();
+
+
+  const updateFilter = (e) => {
+    dispatch(updateFilterStatus(e.target.value))
+  };
 
   return (
     <div className={styles.appHeader}>
       <Button variant='primary' onClick={() => setModalOpen(true)}>
         Add Task
       </Button>
-      <SelectButton id='status'>
+      <SelectButton id='status' value={filterStatus} onChange={updateFilter}>
         <option value='all'>All</option>
         <option value='incomplete'>Incomplete</option>
         <option value='complete'>complete</option>
